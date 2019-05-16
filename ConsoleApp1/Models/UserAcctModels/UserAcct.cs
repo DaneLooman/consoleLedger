1172a31f7f2b1e74c1d7df34a06b15796a55a6c7
Howard Looman <hlooman@mix.wvu.edu>
2019-05-16 12:37:00 -0400

    Basic log-in functionallity created (needs refactored)

---
+++ ConsoleApp1/Models/UserAcctModels/UserAcct.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1.Models.UserAcctModels
+{
+    class UserAcct
+    {
+        public int UserAcctId { get; set; }
+        public string UserAcctPassword { get; set; }
+        public string UserAcctName { get; set; }
+        public int UserBankAcct { get; set; }
+
+    }
+}
--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using ConsoleApp1.Models.UserAcctModels;
+using System;
+using System.Collections;
+using System.Collections.Generic;
 
 namespace ConsoleApp1
 {
@@ -6,7 +9,67 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<UserAcct> userAccts = new List<UserAcct>();
+            UserAcct currentUser = new UserAcct();
+            bool loggedIn = false; 
+            while (loggedIn == false){
+                Console.WriteLine("Type your email address to log in or \"new\" if you are a new user");
+                string result = Console.ReadLine().ToLower();
+                if (result == "new"){
+                    Console.WriteLine("Please enter your full Email Address");
+                    string emailResult = Console.ReadLine().Trim().ToLower();
+                    if (userAccts.FindIndex(u => u.UserAcctName == emailResult) < 0) {
+                        currentUser.UserAcctName = emailResult;
+                            Console.WriteLine("Please enter a passowrd");
+                            string passResult1 = Console.ReadLine();
+
+                            Console.WriteLine("Please re-enter a passowrd");
+                            string passResult2 = Console.ReadLine();
+
+                            if (passResult1 == passResult2) {
+                            }
+                            else {
+                                while (passResult1 != passResult2) { 
+                                    Console.WriteLine("Sorry. Your passowrd did not match. Please try again.");
+                                    Console.WriteLine("Please enter a passowrd");
+                                    passResult1 = Console.ReadLine();
+
+                                    Console.WriteLine("Please re-enter a passowrd");
+                                    passResult2 = Console.ReadLine();
+                                }
+                            }
+                            currentUser.UserAcctPassword = passResult1;
+                            currentUser.UserAcctId = userAccts.Count + 1;
+                            userAccts.Add(currentUser);
+                           }
+                        else {
+                            Console.WriteLine("This user already exists.");
+                        }
+                }
+                else if (userAccts.FindIndex(u => u.UserAcctName == result) >= 0){
+                    int attempts = 0;
+                    UserAcct attemptUser = userAccts.Find(u => u.UserAcctName == result);
+                    string attemptPass;
+                    Console.WriteLine("Please Enter Your Password");
+                    attemptPass = Console.ReadLine();
+                    while(attemptPass != attemptUser.UserAcctPassword && attempts < 3) {                        
+                        Console.WriteLine("Sorry. That password does not match what we have on file. Please enter your password again.");
+                        attemptPass = Console.ReadLine();
+                        attempts++;
+                    }
+                    if (attemptPass != attemptUser.UserAcctPassword && attempts >= 3){
+                        Console.WriteLine("Sorry. Maximum attempts (3) exceeded.");
+                    } else {
+                        currentUser = attemptUser;
+                        loggedIn = true;
+                    }
+                }
+                else {
+                    Console.WriteLine("Sorry. We could not find a user with that email address. Please try again.");
+                }          
+            }
+            Console.WriteLine(currentUser);
+            Console.Read();
         }
     }
 }
