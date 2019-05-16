2e05c200c6ff91966b8bbc1a9a97caba2fd76e9d
Howard Looman <hlooman@mix.wvu.edu>
2019-05-16 14:46:48 -0400

    Added Transaction Class

---
+++ ConsoleApp1/Models/UserTransactionModels/Transactions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1.Models.UserTransactionModels
+{
+    class Transactions
+    {
+        public int Id { get; set; }
+        public string Memo { get; set; }
+        public decimal Amt { get; set; }
+        public int  UserId { get; set; }
+    }
+}
