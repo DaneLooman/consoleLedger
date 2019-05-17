51226cebf838282357ddf23fe22f01e9ee0d1a26
Howard Looman <hlooman@mix.wvu.edu>
2019-05-17 15:31:48 -0400

    cleaned up text and refactored start menu.

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -9,6 +9,138 @@ namespace ConsoleApp1
     class Program
     {
         //<------------Methods------------------------------------->
+        //Start Menu (create account, log in and run main menu, or shut down program)
+
+       static Tuple<bool, List<User>, List<Transaction>> Start(List<User> users, List<Transaction> transactions)
+        {
+            User currentUser = new User();
+            Console.Clear();
+            Console.WriteLine("Chose an option:\n1: Create New Account\n2: Log In\n3: Shut Down");
+            int selection;
+            bool success = Int32.TryParse(Console.ReadLine(), out selection);
+            if (success)
+            {
+                if (selection == 1)
+                {
+                    Console.WriteLine("Please enter your full Email Address");
+                    string emailResult = Console.ReadLine().Trim().ToLower();
+
+                    if (users.FindIndex(u => u.UserAcctName == emailResult) < 0)
+                    {
+                        currentUser.UserAcctName = emailResult;
+                        Console.WriteLine("Please enter a password");
+                        string passResult1 = Console.ReadLine();
+
+                        Console.WriteLine("Please re-enter a password");
+                        string passResult2 = Console.ReadLine();
+
+                        if (passResult1 == passResult2)
+                        {
+                        }
+                        else
+                        {
+                            while (passResult1 != passResult2)
+                            {
+                                Console.WriteLine("Sorry. Your password did not match. Please try again.");
+                                Console.WriteLine("Please enter a password");
+                                passResult1 = Console.ReadLine();
+
+                                Console.WriteLine("Please re-enter a password");
+                                passResult2 = Console.ReadLine();
+                            }
+                        }
+                        currentUser.UserAcctPassword = passResult1;
+                        currentUser.UserAcctId = users.Count + 1;
+                        users.Add(currentUser);
+                        return new Tuple<bool, List<User>, List<Transaction>>(true, users, transactions);
+
+                    }
+                    else
+                    {
+                        Console.WriteLine("This user already exists. Press Enter to restart.");
+                        Console.ReadLine();
+                        return new Tuple<bool, List<User>, List<Transaction>>(true, users, transactions);
+                    }
+
+                }
+                else if (selection == 2)
+                {
+                    bool loggedIn = false;
+                    Console.Clear();
+                    Console.WriteLine("Type your email address to log in.");
+                    string result = Console.ReadLine().ToLower();
+                    if (users.FindIndex(u => u.UserAcctName == result) >= 0)
+                    {
+                        int attempts = 0;
+                        User attemptUser = users.Find(u => u.UserAcctName == result);
+                        string attemptPass;
+                        Console.WriteLine("Please Enter Your Password");
+                        attemptPass = Console.ReadLine();
+
+                        while (attemptPass != attemptUser.UserAcctPassword && attempts < 3)
+                        {
+                            Console.WriteLine("Sorry. That password does not match what we have on file. Please enter your password again.");
+                            attemptPass = Console.ReadLine();
+                            attempts++;
+                        }
+
+                        if (attemptPass != attemptUser.UserAcctPassword && attempts >= 3)
+                        {
+                            Console.WriteLine("Sorry. Maximum attempts (3) exceeded.");
+                        }
+                        else
+                        {
+                            currentUser = attemptUser;
+                            loggedIn = true;
+                        }
+                        while (loggedIn == true)
+                        {
+                            var results = (MainMenu(currentUser, transactions));
+                            transactions = results.Item2;
+                            loggedIn = results.Item1;
+                        }
+                        Console.WriteLine("You have logged out. Press Enter to return to the Main Menu.");
+                        Console.ReadLine();
+                        return new Tuple<bool, List<User>, List<Transaction>>(true, users, transactions);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Sorry. We could not find a user with that email address. Press Enter to try again.");
+                        Console.ReadLine();
+                        return new Tuple<bool, List<User>, List<Transaction>>(true, users, transactions);
+                    }
+                }
+                else if (selection == 3)
+                {
+                    Console.WriteLine("Are you sure you want to shut down? Y/N?");
+                    string result = Console.ReadLine().ToLower();
+                    if (result == "y")
+                    {
+                        return new Tuple<bool, List<User>, List<Transaction>>(false, users, transactions);
+                    } else
+                    {
+                        Console.WriteLine("Shut Down Cancelled. Press Enter to restart");
+                        Console.ReadLine();
+                        return new Tuple<bool, List<User>, List<Transaction>>(true, users, transactions);
+                    }
+
+                }
+                else {
+                    Console.WriteLine("Please only enter numbers 1, 2, or 3. Press Enter to restart");
+                    Console.ReadLine();
+                    return new Tuple<bool, List<User>, List<Transaction>>(true, users, transactions);
+                }
+
+            }
+            else
+            {
+                Console.WriteLine("Please only enter numbers 1, 2, or 3. Press Enter to restart");
+                Console.ReadLine();
+            }
+            return new Tuple<bool, List<User>, List<Transaction>>(true, users, transactions);
+        }
+            
+         
         //Main Menu method returns a bool for if the user is logged in and returns a new version of the transaction list
         static Tuple<bool, List<Transaction>> MainMenu(User user, List<Transaction> _transactions)
         {
@@ -182,7 +314,7 @@ namespace ConsoleApp1
             }
         }
 
-//----------------------Main Program---------------------------//
+        //----------------------Main Program---------------------------//
         static void Main(string[] args)
         {
             List<User> userAccts = new List<User>();
@@ -191,79 +323,18 @@ namespace ConsoleApp1
             userAccts = seedResult.Item1;
             transactions = seedResult.Item2;
             User currentUser = new User();
-            bool loggedIn = false; 
+            bool running = true;
 
-            //allows for logging in and account creation. Once completed flips loggedIn to true and sets current User
-            while (loggedIn == false){
-                Console.Clear();
-                Console.WriteLine("Type your email address to log in or \"new\" if you are a new user");
-                string result = Console.ReadLine().ToLower();
-
-                if (result == "new"){
-                    Console.WriteLine("Please enter your full Email Address");
-                    string emailResult = Console.ReadLine().Trim().ToLower();
-
-                    if (userAccts.FindIndex(u => u.UserAcctName == emailResult) < 0) {
-                        currentUser.UserAcctName = emailResult;
-                            Console.WriteLine("Please enter a password");
-                            string passResult1 = Console.ReadLine();
-
-                            Console.WriteLine("Please re-enter a password");
-                            string passResult2 = Console.ReadLine();
-
-                            if (passResult1 == passResult2) {
-                            }
-                            else {
-                                while (passResult1 != passResult2) { 
-                                    Console.WriteLine("Sorry. Your password did not match. Please try again.");
-                                    Console.WriteLine("Please enter a password");
-                                    passResult1 = Console.ReadLine();
-
-                                    Console.WriteLine("Please re-enter a password");
-                                    passResult2 = Console.ReadLine();
-                                }
-                            }
-                            currentUser.UserAcctPassword = passResult1;
-                            currentUser.UserAcctId = userAccts.Count + 1;
-                            userAccts.Add(currentUser);
-                           }
-                        else {
-                            Console.WriteLine("This user already exists.");
-                        }
-                }
-                else if (userAccts.FindIndex(u => u.UserAcctName == result) >= 0){
-                    int attempts = 0;
-                    User attemptUser = userAccts.Find(u => u.UserAcctName == result);
-                    string attemptPass;
-                    Console.WriteLine("Please Enter Your Password");
-                    attemptPass = Console.ReadLine();
-
-                    while(attemptPass != attemptUser.UserAcctPassword && attempts < 3) {                        
-                        Console.WriteLine("Sorry. That password does not match what we have on file. Please enter your password again.");
-                        attemptPass = Console.ReadLine();
-                        attempts++;
-                    }
-
-                    if (attemptPass != attemptUser.UserAcctPassword && attempts >= 3){
-                        Console.WriteLine("Sorry. Maximum attempts (3) exceeded.");
-                    } else {
-                        currentUser = attemptUser;
-                        loggedIn = true;
-                    }
-                }
-                else {
-                    Console.WriteLine("Sorry. We could not find a user with that email address. Please try again.");
-                }          
-            }
-            while (loggedIn == true)
+            while (running == true)
             {
-                var results = (MainMenu(currentUser, transactions));
-                transactions = results.Item2;
-                loggedIn = results.Item1;
+                var results = Start(userAccts, transactions);
+                userAccts = results.Item2;
+                transactions = results.Item3;
+                running = results.Item1;
             }
 
-            Console.WriteLine("You have logged out");
-            Console.Read();
+            Console.WriteLine("Goodbye!");
+            Console.ReadLine();
         }
     }
 }
